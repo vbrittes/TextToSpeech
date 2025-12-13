fbfc11e408aad6cc54ed202c629e0af73e95134a
Victor Brittes <victor.brittes@fuze.cc>
2025-12-13 07:12:03 -0300

    first commit

---
+++ TextToSpeech/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TextToSpeech
+//
+//  Created by Victor Brittes on 13/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TextToSpeech/TextToSpeechApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextToSpeechApp.swift
+//  TextToSpeech
+//
+//  Created by Victor Brittes on 13/12/25.
+//
+
+import SwiftUI
+
+@main
+struct TextToSpeechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
